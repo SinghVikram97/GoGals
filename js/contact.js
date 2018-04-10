b1cd3fd934961c3442b798941403e2eee784e05a
Priyabrata Biswas <pbiswas101b@gmail.com>
2018-04-10 08:46:57 +0530

    scripts for contact page added

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+window.onload = init;
+
+function init() {
+    var typed = new Typed('.typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 100,
+        backDelay: 1000,
+        backSpeed: 100,
+        loop: true
+    });
+}
